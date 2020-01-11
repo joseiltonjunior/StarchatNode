93d9a063ee1bcade58d89516ff0165d688e7eff5
joseiltonjunior <joseiltonjuniortech@gmail.com>
2020-01-11 19:27:09 -0300

    Lógica finalizada e algumas personalizações

--- server.js
+++ server.js
@@ -19,9 +19,13 @@ let messages = []
 io.on('connection', socket => {
     console.log(`Socket conectado: ${socket.id}`)
 
+    socket.emit('previousMessages', messages)
+
     socket.on('sendMessage', data => {
         messages.push(data)
+        socket.broadcast.emit('receivedMessage', data)
     })
 })
+
 
 server.listen(3000)
